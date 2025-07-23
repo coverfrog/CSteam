e2bb16917cd958c85fb845a19231c74084dfbb47
coverfrog <justfroglab@gmail.com>
2025-07-24 08:15:04 +0900

    Init

---
+++ CSteam/Runtime/Steam/SteamManager_Method.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Steamworks;
+using Steamworks.Data;
+using UnityEngine.SceneManagement;
+
+namespace Cf.CSteam
+{
+    public partial class SteamManager
+    {
+        public void LoadScene(string sceneName, LoadSceneMode loadSceneMode)
+        {
+            Net.SceneManager.LoadScene(sceneName, loadSceneMode);
+        }
+        
+        public async void StartHost(int count = 4)
+        {
+            await SteamMatchmaking.CreateLobbyAsync(count);
+        }
+
+        public void Leave()
+        {
+            Net.Shutdown();
+        }
+        
+        public async void JoinWithId(ulong id)
+        {
+            Lobby[] lobbyArr = await SteamMatchmaking.LobbyList.WithSlotsAvailable(1).RequestAsync();
+
+            Lobby lobby = lobbyArr.FirstOrDefault(l => l.Id == id);
+
+            await lobby.Join();
+        }
+
+        public void SendChat(string message)
+        {
+            CurrentLobby.SendChatString(message);
+        }
+    }
+}
---
+++ CSteam/Runtime/Steam/SteamManager_Sub.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Steamworks;
+using UnityEngine.SceneManagement;
+
+namespace Cf.CSteam
+{
+    public partial class SteamManager
+    {
+        private static void SubSteam()
+        {
+            SteamMatchmaking.OnLobbyCreated += LobbyCreated;
+            SteamMatchmaking.OnLobbyEntered += LobbyEntered;
+            SteamMatchmaking.OnLobbyMemberJoined += LobbyMemberJoined;
+            SteamMatchmaking.OnLobbyMemberLeave += LobbyMemberLeave;
+            SteamMatchmaking.OnChatMessage += ChatMessaged;
+            
+            SteamFriends.OnGameLobbyJoinRequested += GameLobbyJoinRequested;
+        }
+
+        private static void SubScene()
+        {
+            Net.SceneManager.OnLoadEventCompleted += SceneLoadEventCompleted;
+        }
+    }
+}
