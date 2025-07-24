100a7be03471bf27822702f6fa6673cbc7d2b7ac
DESKTOP-H9T1FUJ\lockt <coverfrog@gmail.com>
2025-07-25 07:38:20 +0900

    Network Manager Create Option Inspector

--- Assets/CSteam/Runtime/Steam/SteamManager.cs
+++ Assets/CSteam/Runtime/Steam/SteamManager.cs
@@ -13,6 +13,9 @@ namespace Cf.CSteam
 {
     public partial class SteamManager : NetworkBehaviour
     {
+        [SerializeField] private string netPrefabPath = "NetworkManager";
+        [SerializeField] private float netSingletonTimeOut = 0.1f;
+        
         private static NetworkManager Net { get; set; }
 
         private static FacepunchTransport Transport { get; set; }
@@ -48,14 +51,11 @@ namespace Cf.CSteam
 
         private IEnumerator CoLoadNetworkManager()
         {
-            const float timeOut = 0.5f;
-            const string path = "Network Manager";
-            
             float timer = 0.0f;
 
             _ = NetworkManager.Singleton;
             
-            while (!NetworkManager.Singleton && timer < timeOut)
+            while (!NetworkManager.Singleton && timer < netSingletonTimeOut)
             {
                 timer += Time.deltaTime;
                 
@@ -67,11 +67,20 @@ namespace Cf.CSteam
                 yield break;
             }
 
-            GameObject obj = (GameObject)Instantiate(Resources.Load(path), Vector3.zero, Quaternion.identity);
+            Object resource = Resources.Load(netPrefabPath);
+
+            if (resource == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Not Found Prefab");
+#endif
+                yield break;
+            }
+            
+            GameObject obj = (GameObject)Instantiate(resource, Vector3.zero, Quaternion.identity);
             
             Net = obj.GetComponent<NetworkManager>();
             Transport = obj.GetComponent<FacepunchTransport>();
-            
         }
     }
 }
